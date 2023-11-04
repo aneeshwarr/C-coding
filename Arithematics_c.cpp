e4985dbe39d0cdbab8a0855381d28f4cf0c59d56
aneeshwarr <149313321+aneeshwarr@users.noreply.github.com>
2023-11-04 19:36:56 -0400

    Arithmetic

---
+++ Arithematics_c.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int x,y,z;
+	printf("Enter 2 values");
+	scanf("%d%d",&x,&y);
+	printf("x+y=%d\n",x+y);
+	printf("x-y=%d\n",x-y);
+	printf("x*y=%d\n",x*y);
+	printf("x/y=%d\n",x/y);
+	printf("x%y=%d\n",x%y);
+}
