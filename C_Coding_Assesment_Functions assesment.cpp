ed325068eb7ef2697105bd7936d3416d08d968fc
aneeshwarr <149313321+aneeshwarr@users.noreply.github.com>
2024-01-26 21:53:18 -0500

    Update C_Coding_Assesment_Functions assesment.cpp

--- C_Coding_Assesment_Functions assesment.cpp
+++ C_Coding_Assesment_Functions assesment.cpp
@@ -1,18 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
-void display1(int a,int b)
+void Addition(int a,int b)
 {
 	printf("The Sum of %d and %d = %d\n",a,b,a+b);
 }
-void display2(int a,int b)
+void Difference(int a,int b)
 {
 	printf("The difference of %d and %d = %d\n",a,b,a-b);
 }
-void display3(int a,int b)
+void Multiplication(int a,int b)
 {
 	printf("The product of %d and %d = %d\n",a,b,a*b);
 }
-void display4(int a,int b)
+void Division(int a,int b)
 {
 	printf("The quotient of %d and %d = %d with remainder %d\n",a,b,a/b,a%b);
 }
@@ -21,7 +21,7 @@ int main()
 	int choice,x,z;
 	while(1)
 	{
-	printf("\n1. Addition\n2.Difference\n3.Multiplication\n4.Division\n5.All\n6.Exit");
+	printf("\n1. Addition\n2.Difference\n3.Multiplication\n4.Difference\n5.All\n6.Exit");
 	printf("\nEnter your choice(with the number): ");
 	scanf("%d",&choice);
 	if(choice!=6)
@@ -32,22 +32,22 @@ int main()
 	switch(choice)
 	{
 		case 1:
-			display1(x,z);
+			Addition(x,z);
 			break;
 		case 2:
-			display2(x,z);
+			Difference(x,z);
 			break;
 		case 3:
-			display3(x,z);
+			Multiplication(x,z);
 			break;
 		case 4:
-			display4(x,z);
+			Division(x,z);
 			break;
 		case 5:
-			display1(x,z);
-			display2(x,z);
-			display3(x,z);
-			display4(x,z);
+			Addition(x,z);
+			Difference(x,z);
+			Multiplication(x,z);
+			Division(x,z);
 		case 6:
 			exit(0);
 	}
